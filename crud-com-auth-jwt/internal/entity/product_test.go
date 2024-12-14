bed6a24fa1ee6dd84f65681f4621a17a42e4e08a
Marlliton Souza <marlliton.souza1@gmail.com>
2024-12-13 23:30:59 -0300

    teste de produto

--- crud-com-auth-jwt/internal/entity/product_test.go
+++ crud-com-auth-jwt/internal/entity/product_test.go
@@ -7,16 +7,16 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	p, err := NewProduct("Product 1", 99)
+	p, err := NewProduct("Product 1", 99.00)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, "Product 1", p.Name)
-	assert.Equal(t, 99, p.Price)
+	assert.Equal(t, 99.00, p.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	p, err := NewProduct("", 99)
+	p, err := NewProduct("", 99.00)
 	assert.NotNil(t, err)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrNameIsRequired, err)
